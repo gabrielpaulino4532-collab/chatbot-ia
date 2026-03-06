f730d15f0c9961f658e58eb1e820c65f667e8e96
operator01154 <gabrielpaulino4532@gmail.com>
2026-03-05 23:46:32 -0300

    Implement chatbot functionality with Groq API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,38 @@
 import streamlit as st
+from groq import Groq
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+modelo_ia = Groq(api_key="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")
+
+st.write("# Chatbot com IA")
+
+if not "lista_mensagens" in st.session_state:
+    st.session_state["lista_mensagens"] = []
+
+texto_usuario = st.chat_input("Digite sua mensagem aqui...")
+
+for mensagem in st.session_state["lista_mensagens"]:
+    role = mensagem["role"]
+    content = mensagem["content"]
+    st.chat_message(role).write(content)
+
+if texto_usuario:
+    print(texto_usuario)
+    st.chat_message("user").write(texto_usuario)
+    mensagem_usuario = {"role": "user", "content": texto_usuario}
+    st.session_state["lista_mensagens"].append(mensagem_usuario)
+    # Nome
+    # User (usuário normal)
+    # Assistant (assistente, o chatbot)
+
+    resposta_ia = modelo_ia.chat.completions.create(
+        messages=[
+            {"role": "system", "content": "Você é um assistente útil e amigável."},
+            *st.session_state["lista_mensagens"]
+    ],
+        model="openai/gpt-oss-120b"
+    )
+    texto_reposta_ia = resposta_ia.choices[0].message.content
+    
+    st.chat_message("assistant").write(texto_reposta_ia)
+    mensagem_ia = {"role": "assistant", "content": texto_reposta_ia}
+    st.session_state["lista_mensagens"].append(mensagem_ia)
